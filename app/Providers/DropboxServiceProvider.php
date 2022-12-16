91538b5f25319ecea34c67c5b76c855041872047
Borwoll <borwoll@borwoll.github.io>
2022-12-16 11:58:33 +0300

    Changes to be committed: 	modified:   app/Providers/DropboxServiceProvider.php

--- app/Providers/DropboxServiceProvider.php
+++ app/Providers/DropboxServiceProvider.php
@@ -9,15 +9,8 @@ use League\Flysystem\Filesystem;
 use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
 
-class DropboxServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+class DropboxServiceProvider extends ServiceProvider {
+    public function boot() {
         Storage::extend('dropbox', function ($app, $config) {
             $client = new DropboxClient(
                 $config['authorization_token']
